bedc1b830c080adf4ef39f9c7353ee70cfadeeff
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-02-11 01:13:19 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const string = n.toString().replace('-', '')
+  res = ''
+  for (let i = string.length - 1; i >= 0; i--) {
+    res += string[i]
+  }
+
+  return res
 }
